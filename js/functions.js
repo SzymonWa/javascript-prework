1c10cb6485d3f7cabf4ac7a2e7bcdd30b987899a
Szymon Walczak <szym692@gmail.com>
2023-02-04 05:35:20 +0100

    Add random answers

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,20 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} 
+else if(randomNumber == '2'){
+    computerMove = 'papier';
+}
+else if(randomNumber == '3'){
+    computerMove = 'nożyce';
+}
+else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
